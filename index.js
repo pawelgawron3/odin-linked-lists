5cddf6a15fcbd24fa3716414b11bf8374a819524
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-09-27 13:01:23 +0200

    Test the new methods

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import { LinkedList } from "./linkedList.js";
 
 let linkedList = new LinkedList();
 
-console.log(linkedList.size());
 linkedList.prepend(10);
 linkedList.append(20);
+linkedList.append(30);
 console.log(linkedList.size());
+console.log(linkedList.node());
+console.log(linkedList.tail());
