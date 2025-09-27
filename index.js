52b6fd300885c14eae6ae04417432928ab9921d6
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-09-27 15:26:36 +0200

    Delete the unnecessary lines of code

--- index.js
+++ index.js
@@ -3,16 +3,3 @@ import { LinkedList } from "./linkedList.js";
 let linkedList = new LinkedList();
 
 console.log(linkedList.size());
-linkedList.prepend(10);
-linkedList.append(20);
-linkedList.append(30);
-linkedList.append(40);
-linkedList.insertAt(100, 2);
-console.log(linkedList.toString());
-linkedList.removeAt(2);
-console.log(linkedList.toString());
-console.log(linkedList.find(30));
-console.log(linkedList.size());
-linkedList.pop();
-console.log(linkedList.size());
-console.log(linkedList.find(30));
